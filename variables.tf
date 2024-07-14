16674f3b0bba8009fb663ebd8f37af4bde2dd6dd
dzionis <dziioniis@gmail.com>
2024-07-14 18:10:03 +0400

    Update variable prefix to project_prefix in Terraform scripts

--- variables.tf
+++ variables.tf
@@ -14,10 +14,15 @@ variable "db_instance_class" {
   type = string
 }
 
+variable "project_prefix" {
+  type = string
+}
+
 locals {
   region = var.region
   project_name = var.project_name
-  prefix = "${terraform.workspace}-${local.project_name}"
+  project_prefix = var.project_prefix
+  resource_prefix = "${terraform.workspace}-${local.project_prefix}"
   database_username = var.database_username
   db_instance_class = var.db_instance_class
 }
